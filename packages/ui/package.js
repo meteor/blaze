63a5d06a6da13b3aba9b70870b298d99fec64e2c
Mitar <mitar.git@tnode.com>
2016-09-13 22:01:50 -0700

    Making stand-alone.

--- packages/ui/package.js
+++ packages/ui/package.js
@@ -1,16 +1,24 @@
 Package.describe({
+  name: 'ui',
   summary: "Deprecated: Use the 'blaze' package",
-  version: '1.0.11'
+  version: '1.0.11',
+  git: 'https://github.com/meteor/blaze.git'
 });
 
 Package.onUse(function (api) {
-  api.use('blaze');
-  api.imply('blaze');
+  api.versionsFrom('METEOR@1.4.1');
+
+  api.use('blaze@2.1.9');
+  api.imply('blaze@2.1.9');
 
   // XXX COMPAT WITH PACKAGES BUILT FOR 0.9.0.
   //
   // (in particular, packages that have a weak dependency on this
   // package, since then exported symbols live on the
   // `Package.ui` object)
-  api.export(['Blaze', 'UI', 'Handlebars']);
+  api.export([
+    'Blaze',
+    'UI',
+    'Handlebars'
+  ]);
 });
