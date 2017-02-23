3e62093b3709bbb59bd7954adf44dff0a8f65df6
Seth Murphy <smurphy@eagerstwolf.com>
2017-02-23 09:21:06 -0500

    Updated templating-tools to require uglify from npm

--- packages/spacebars-compiler/compiler.js
+++ packages/spacebars-compiler/compiler.js
@@ -95,22 +95,18 @@ SpacebarsCompiler.codeGen = function (parseTree, options) {
 };
 
 SpacebarsCompiler._beautify = function (code) {
-  if (Package['minifier-js'] && Package['minifier-js'].UglifyJSMinify) {
-    var result = Package['minifier-js'].UglifyJSMinify(
-      code,
-      { fromString: true,
-        mangle: false,
-        compress: false,
-        output: { beautify: true,
-                  indent_level: 2,
-                  width: 80 } });
-    var output = result.code;
-    // Uglify interprets our expression as a statement and may add a semicolon.
-    // Strip trailing semicolon.
-    output = output.replace(/;$/, '');
-    return output;
-  } else {
-    // don't actually beautify; no UglifyJS
-    return code;
-  }
+  var UglifyJSMinify = Npm.require('uglify-js').minify;
+  var result = UglifyJSMinify(
+    code,
+    { fromString: true,
+      mangle: false,
+      compress: false,
+      output: { beautify: true,
+                indent_level: 2,
+                width: 80 } });
+  var output = result.code;
+  // Uglify interprets our expression as a statement and may add a semicolon.
+  // Strip trailing semicolon.
+  output = output.replace(/;$/, '');
+  return output;
 };
