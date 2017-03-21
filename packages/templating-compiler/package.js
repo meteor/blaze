6d831adf285bbd156cf6ac9d81501b35a61b3629
Mitar <mitar.github@tnode.com>
2017-03-21 04:48:03 -0700

    Not true anymore.

--- packages/templating-compiler/package.js
+++ packages/templating-compiler/package.js
@@ -8,12 +8,6 @@ Package.describe({
 
 Package.registerBuildPlugin({
   name: "compileTemplatesBatch",
-  // minifier-js is a weak dependency of spacebars-compiler; adding it here
-  // ensures that the output is minified.  (Having it as a weak dependency means
-  // that we don't ship uglify etc with built apps just because
-  // boilerplate-generator uses spacebars-compiler.)
-  // XXX maybe uglify should be applied by this plugin instead of via magic
-  // weak dependency.
   use: [
     'ecmascript@0.5.8',
     'caching-html-compiler@1.1.1',
