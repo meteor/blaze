4b612bcb936879a4d3a90f52d702525a7f2850ca
Jan Dvorak <storytellercz@proton.me>
2023-11-27 16:28:11 +0100

    Update dependencies in blaze-hot
    Update dependencies and replace two `var` with `let`. Also add some missing semi-colons.

--- packages/blaze-hot/package.js
+++ packages/blaze-hot/package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'blaze-hot',
   summary: "Update files using Blaze's API with HMR",
-  version: '1.1.1',
+  version: '1.1.2',
   git: 'https://github.com/meteor/blaze.git',
   documentation: null,
   debugOnly: true
 });
 
 Package.onUse(function (api) {
-  api.use('modules@0.16.0');
-  api.use('ecmascript@0.15.1');
+  api.use('modules@0.19.0');
+  api.use('ecmascript@0.16.7');
   api.use('blaze@2.7.1');
-  api.use('templating-runtime@1.6.0');
-  api.use('hot-module-replacement@0.2.0', { weak: true });
+  api.use('templating-runtime@1.6.3');
+  api.use('hot-module-replacement@0.5.3', { weak: true });
 
   api.addFiles('hot.js', 'client');
   api.addFiles('update-templates.js', 'client');
